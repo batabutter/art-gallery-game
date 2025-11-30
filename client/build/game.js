5fb3a2a65e20f303eea2318442f6e302b3c7260c
bata_butter <113527212+batabutter@users.noreply.github.com>
2025-11-30 02:35:35 -0500

    Fixed line drawing alongside polygon graphics drawing

--- client/build/game.js
+++ client/build/game.js
@@ -81,7 +81,7 @@ var ArtGalleryGame;
             spikiness *= averageRadius;
             var angleSteps = this.randomAngleSteps(numVertices, irregularity);
             var points = [];
-            var angle = this.uniform(2 * Math.PI, 0);
+            var angle = this.uniform(0, 2 * Math.PI);
             for (var i = 0; i < numVertices; i++) {
                 var radius = this.clip(this.gauss(averageRadius, spikiness), 0, 2 * averageRadius);
                 var point = {
@@ -93,23 +93,55 @@ var ArtGalleryGame;
             }
             return points;
         };
+        GalleryGame.prototype.addPhysicsLine = function (point, xOffset, yOffset) {
+            // What the fuck
+            var line = this.add.line(xOffset, yOffset, 0, 0, point.xPos, point.yPos);
+        };
+        GalleryGame.prototype.drawPolygonBounds = function (points) {
+            var bounds = [];
+            if (points.length > 0) {
+                var start = points[0];
+                for (var i = 1; i < points.length; i++) {
+                    var line = this.add.line(0, 0, points[i - 1].xPos, points[i - 1].yPos, points[i].xPos, points[i].yPos, 0x000000).setOrigin(0, 0);
+                    bounds.push(line);
+                }
+                bounds.push(this.add.line(0, 0, points[points.length - 1].xPos, points[points.length - 1].yPos, start.xPos, start.yPos, 0x000000).setOrigin(0, 0));
+            }
+            else {
+                throw new RangeError("Points list must be greater than 0.");
+            }
+            return bounds;
+        };
+        GalleryGame.prototype.drawDebugMarkers = function (points) {
+            for (var _i = 0, points_1 = points; _i < points_1.length; _i++) {
+                var p = points_1[_i];
+                console.log("Drawing marker at: (".concat(p.xPos, ", ").concat(p.yPos, ")"));
+                var pointMarker = this.add.circle(p.xPos, p.yPos, 3, 0xffffff, 1);
+                pointMarker.setStrokeStyle(1, 0x000000);
+            }
+        };
         GalleryGame.prototype.create = function () {
             var xOffset = this.width / 2;
             var yOffset = this.height / 2;
             this.graphics = this.add.graphics();
             this.graphics.lineStyle(5, 0x0, 1.0);
             /* Initialize values for polygons */
+            this.graphics.beginPath();
             //this.borders = this.physics.add.staticGroup();
             var circle = this.add.circle(100, 100, 10, 0x00FFFF);
             makeDraggable(circle, true);
             var points = this.generatePolygon({ xPos: xOffset, yPos: yOffset }, 175, 0.8, 0.3, 16);
             /* Ideal polygon :  */
             this.graphics.lineStyle(2, 0xFF0000, 1.0);
-            for (var i = 0; i < points.length; i++) {
-                this.graphics.lineTo(points[i].xPos, points[i].yPos);
+            console.debug(points);
+            if (points.length > 0) {
+                this.graphics.moveTo(points[0].xPos, points[0].yPos);
+                for (var i = 0; i < points.length; i++) {
+                    this.graphics.lineTo(points[i].xPos, points[i].yPos);
+                }
             }
-            var polygon = this.add.polygon(xOffset, yOffset, points.map(function (v) { return ({ x: v.xPos, y: v.yPos }); }), 0x008000, 0.5);
-            var bounds = polygon.getBounds();
+            var polygonBounds = this.drawPolygonBounds(points);
+            this.drawDebugMarkers(points);
             this.graphics.closePath();
             this.graphics.strokePath();
         };
--- client/src/game.ts
+++ client/src/game.ts
@@ -1,6 +1,6 @@
 /// <reference path="../types/phaser.d.ts"/>
 
-import { makeDraggable } from './draggable.js'; 
+import { makeDraggable } from './draggable.js';
 
 namespace ArtGalleryGame {
 
@@ -8,10 +8,10 @@ namespace ArtGalleryGame {
         xPos: integer;
         yPos: integer;
     }
-    
+
     var p: point = {
-        xPos:100,
-        yPos:100
+        xPos: 100,
+        yPos: 100
     }
 
     interface polygon {
@@ -31,7 +31,7 @@ namespace ArtGalleryGame {
         }
     }
 
-    
+
     export class GalleryGame extends Phaser.Scene {
 
         graphics: Phaser.GameObjects.Graphics | null = null;
@@ -50,12 +50,12 @@ namespace ArtGalleryGame {
 
         }
 
-        private uniform (high, low) {
-            return Math.random() * (high-low) + low;
+        private uniform(high, low) {
+            return Math.random() * (high - low) + low;
         }
 
         private randomAngleSteps(steps: integer, irregularity: number) {
-            var angles:number[] = [];
+            var angles: number[] = [];
 
             var lower = (2 * Math.PI / steps) - irregularity;
             var upper = (2 * Math.PI / steps) + irregularity;
@@ -67,18 +67,18 @@ namespace ArtGalleryGame {
             }
 
             cumSum /= (2 * Math.PI);
-            for(var i = 0; i < steps; i++) {
+            for (var i = 0; i < steps; i++) {
                 angles[i] /= cumSum;
             }
 
             return angles;
         }
 
-        private clip(value:number, lower:number, upper:number) {
+        private clip(value: number, lower: number, upper: number) {
             return Math.min(upper, Math.max(value, lower));
         }
 
-        private gauss(mu:number = 0, sigma:number = 1) {
+        private gauss(mu: number = 0, sigma: number = 1) {
             var u = 1 - Math.random();
             var v = 1 - Math.random();
             return Math.sqrt(-2 * Math.log(u)) * Math.cos(2 * Math.PI * v) * sigma + mu;
@@ -87,25 +87,26 @@ namespace ArtGalleryGame {
         private generatePolygon(
             center: point,
             averageRadius: integer,
-            irregularity: integer, 
-            spikiness: integer, 
+            irregularity: integer,
+            spikiness: integer,
             numVertices: integer,) {
-            if (irregularity < 0 || irregularity > 1) 
+            if (irregularity < 0 || irregularity > 1)
                 throw new RangeError("Irregularity must be between 0 and 1")
-            if (spikiness < 0 || spikiness > 1) 
+            if (spikiness < 0 || spikiness > 1)
                 throw new RangeError("Spikiness must be between 0 and 1")
 
             irregularity *= 2 * Math.PI / numVertices;
             spikiness *= averageRadius;
-            var angleSteps:number[] = this.randomAngleSteps(numVertices, irregularity);
+            var angleSteps: number[] = this.randomAngleSteps(numVertices, irregularity);
             var points: point[] = [];
-            var angle:number = this.uniform(2*Math.PI, 0);
+            var angle: number = this.uniform(0, 2 * Math.PI);
 
             for (var i = 0; i < numVertices; i++) {
-                var radius:number = this.clip(this.gauss(averageRadius, spikiness), 0, 2 * averageRadius);
-                var point:point = {
-                    xPos:(center.xPos + radius * Math.cos(angle)), 
-                    yPos:(center.yPos + radius * Math.sin(angle))};
+                var radius: number = this.clip(this.gauss(averageRadius, spikiness), 0, 2 * averageRadius);
+                var point: point = {
+                    xPos: (center.xPos + radius * Math.cos(angle)),
+                    yPos: (center.yPos + radius * Math.sin(angle))
+                };
                 points.push(point);
                 angle += angleSteps[i];
             }
@@ -113,34 +114,76 @@ namespace ArtGalleryGame {
             return points;
         }
 
+        private drawPolygonBounds(points: point[]) {
+            var bounds = [];
+
+            if (points.length > 0) {
+                var start = points[0];
+                for (var i = 1; i < points.length; i++) {
+                    const line = this.add.line(
+                        0, 0,
+                        points[i - 1].xPos, points[i - 1].yPos,
+                        points[i].xPos, points[i].yPos,
+                        0x000000
+                    ).setOrigin(0, 0);
+                    bounds.push(line);
+                }
+                bounds.push(
+                    this.add.line(
+                        0, 0,
+                        points[points.length-1].xPos, points[points.length-1].yPos,
+                        start.xPos, start.yPos,
+                        0x000000
+                    ).setOrigin(0,0)
+                );
+            } else {
+                throw new RangeError("Points list must be greater than 0.")
+            }
+
+            return bounds;
+        }
+
+        private drawDebugMarkers(points: point[]) {
+            for (var p of points) {
+                console.log(`Drawing marker at: (${p.xPos}, ${p.yPos})`);
+                const pointMarker = this.add.circle(p.xPos, p.yPos, 3, 0xffffff, 1);
+                pointMarker.setStrokeStyle(1, 0x000000);
+            }
+        }
+
         create() {
 
-            const xOffset = this.width/2;
-            const yOffset = this.height/2;
+            const xOffset = this.width / 2;
+            const yOffset = this.height / 2;
 
             this.graphics = this.add.graphics();
             this.graphics.lineStyle(5, 0x0, 1.0);
             /* Initialize values for polygons */
+            this.graphics.beginPath();
 
             //this.borders = this.physics.add.staticGroup();
-            
+
             var circle = this.add.circle(100, 100, 10, 0x00FFFF);
             makeDraggable(circle, true);
 
-            var points = this.generatePolygon({xPos:xOffset, yPos:yOffset}, 175, 0.8, 0.3, 16);
+            var points = this.generatePolygon({ xPos: xOffset, yPos: yOffset }, 175, 0.8, 0.3, 16);
 
             /* Ideal polygon :  */
             this.graphics.lineStyle(2, 0xFF0000, 1.0);
+            console.debug(points);
 
-            for (var i = 0; i < points.length; i++) {
-                this.graphics.lineTo(points[i].xPos, points[i].yPos);
+            if (points.length > 0) {
+                this.graphics.moveTo(points[0].xPos, points[0].yPos);
+                for (var i = 0; i < points.length; i++) {
+                    this.graphics.lineTo(points[i].xPos, points[i].yPos);
+                }
             }
 
-            var polygon = this.add.polygon(xOffset, yOffset, points.map(v => ({x:v.xPos, y:v.yPos})), 0x008000, 0.5);
-            var bounds=  polygon.getBounds();
+            var polygonBounds = this.drawPolygonBounds(points);
+            this.drawDebugMarkers(points);
+
             this.graphics.closePath();
             this.graphics.strokePath();
-
         }
 
 
@@ -151,7 +194,7 @@ namespace ArtGalleryGame {
 var game = new Phaser.Game(
     {
         type: Phaser.AUTO,
-        dom: {createContainer  : true},
+        dom: { createContainer: true },
         parent: "game-wrapper",
         width: 800,
         height: 600,
